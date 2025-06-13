48586799bcd9d504366dae42d69a022d832f3546
Josep Ramon Aleixandri <jraleixandri@iesebre.com>
2025-06-13 18:44:11 +0200

    primer commit

--- f1/views.py
+++ f1/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django import forms
 
-# Create your views here.
+from .models import *
+
+def index(request):
+    latest_escuderies_list = Escuderia.objects.order_by("nom")[:5]
+    context = {"latest_escuderies_list": latest_escuderies_list}
+    return render(request, "index.html", context)
+
+def escuderia(request, escuderia_nom):
+    escuderia = get_object_or_404(Escuderia, pk=escuderia_nom)
+    return render(request, "escuderia.html", {"escuderia": escuderia})
+
+class EscuderiaForm(forms.ModelForm):
+    class Meta:
+        model = Escuderia
+        exclude = ()
+
+def crea_escuderia(request):
+    form = EscuderiaForm()
+
+    # si hi ha dades, les processem
+    if request.method == "POST":
+        form = EscuderiaForm(request.POST)
+        if form.is_valid():
+            escuderia = Escuderia.objects.filter(nom=form.cleaned_data.get("nom"))
+            if escuderia.count()>0:
+                return HttpResponse("ERROR: el nom de l'equip ja existeix.")
+            form.save()
+
+    # creem form si no hi ha dades
+    return render(request,"crea_equip.html",{
+        "form":form,
+        })
+
--- projecte3/urls.py
+++ projecte3/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from f1 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
+    path('crea_equip', views.crea_escuderia ),
 ]
